532e166eb179e989062403698850268769eb4d5e
Suv <gargsuveer@gmail.com>
2018-04-03 19:45:28 +0530

    Motor Driver

--- Motor_Driver/Motor_Driver.ino
+++ Motor_Driver/Motor_Driver.ino
@@ -1,12 +1,15 @@
-#define M1_PWM1 9 
-#define M1_PWM2 10
-#define M1_DIR1 12
-#define M1_DIR2 13
+#define M1_PWM 9 
+#define M2_PWM 10
+#define M1_DIR 12
+#define M2_DIR 13
 
-void Clockwise();
-void AntiClockwise();
-void Speed(int pwm);
+void Clockwise(int pin);
+void AntiClockwise(int pin);
+void Speed(int pwm,int pin);
+void Forward(int pwm);
+void Left_Right(int pwml,int pwmr);
 void brake();
+
 void setPwmFrequency(int pin, int divisor) {
   byte mode;
   if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
@@ -40,38 +43,63 @@ void setPwmFrequency(int pin, int divisor) {
 
 void setup()
 {
-  pinMode(M1_DIR1,OUTPUT);
-  pinMode(M1_DIR2,OUTPUT);
-  pinMode(M1_PWM1,OUTPUT);
-  pinMode(M1_PWM2,OUTPUT);
+  pinMode(M1_DIR,OUTPUT);
+  pinMode(M2_DIR,OUTPUT);
+  pinMode(M1_PWM,OUTPUT);
+  pinMode(M2_PWM,OUTPUT);
   pinMode(13,OUTPUT);
   setPwmFrequency(9, 8); //9,8 ...3.9Khz
+  setPwmFrequency(10, 8);
   delay(1000);
-  analogWrite(9,200);
+  analogWrite(9,50);
+  analogWrite(10,50);
 }
 
 void loop()
 {
-  //digitalWrite(13,HIGH);
-  //delay(1000);
-  digitalWrite(13,LOW);
-  //delay(1000);
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(12,LOW);
+  brake();
+  delay(1000);
+  
 }
 
-void Clockwise()
+void Clockwise(int pin)
 {
-  digitalWrite(M1_DIR1,HIGH);
-  digitalWrite(M1_DIR2,LOW);
+  digitalWrite(pin,HIGH);
 }
 
-void AntiClockwise()
+void AntiClockwise(int pin)
 {
-  digitalWrite(M1_DIR1,LOW);
-  digitalWrite(M1_DIR2,HIGH);
+  digitalWrite(pin,LOW);
 }
 
-void Speed(int pwm)
+void Speed(int pwm,int pin)
 {
-  analogWrite(M1_PWM2, pwm);
+  analogWrite(pin, pwm);
+}
+
+void Forward(int pwm)
+{
+  Clockwise(M1_DIR);
+  Speed(pwm,M1_DIR);
+  Clockwise(M2_DIR);
+  Speed(pwm,M2_DIR);
+}
+
+void Left_Right(int pwml,int pwmr)
+{
+  Clockwise(M1_DIR);
+  Speed(pwml,M1_DIR);
+  Clockwise(M2_DIR);
+  Speed(pwmr,M2_DIR);
+}
+
+
+void brake()
+{
+  Speed(0,M1_DIR);
+  Speed(0,M2_DIR);
 }
 
